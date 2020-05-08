000a79e7db009ab0014edacf0243446e8cc647ae
lbarata <luis_barata@intuit.com>
2020-05-07 21:41:48 -0600

    Updates

---
+++ precache-manifest.95da9a81359eab4e54fc85e19ea1e71d.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a351aee002e5064f06e588f7157853fb",
+    "url": "/codegame/index.html"
+  },
+  {
+    "revision": "66e225e59da68139b0a1",
+    "url": "/codegame/static/css/main.cc5db571.chunk.css"
+  },
+  {
+    "revision": "ef410aeae856d9c66835",
+    "url": "/codegame/static/js/2.ee5027b6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/codegame/static/js/2.ee5027b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66e225e59da68139b0a1",
+    "url": "/codegame/static/js/main.5ab0dee8.chunk.js"
+  },
+  {
+    "revision": "d392c36b083f9dca9e8e",
+    "url": "/codegame/static/js/runtime-main.8a3069dc.js"
+  },
+  {
+    "revision": "b3a492c73c99385c889708af3d8c8eda",
+    "url": "/codegame/static/media/card.b3a492c7.png"
+  },
+  {
+    "revision": "0d3b871b95c39365285d07d633ae7a47",
+    "url": "/codegame/static/media/card_blue.0d3b871b.png"
+  },
+  {
+    "revision": "ef6813f995a579b898da117b9100ca37",
+    "url": "/codegame/static/media/card_bomb.ef6813f9.png"
+  },
+  {
+    "revision": "82ef2a3573a0b1290624f9a8b8717f5c",
+    "url": "/codegame/static/media/card_red.82ef2a35.png"
+  },
+  {
+    "revision": "7ea26672d14e5f6ab7eb34cf5743319a",
+    "url": "/codegame/static/media/card_yellow.7ea26672.png"
+  },
+  {
+    "revision": "8262057119c963741b868e0d8dd68dd3",
+    "url": "/codegame/static/media/economica-bold.82620571.ttf"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/codegame/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "cf4449c1884598b292630aa9f4007c19",
+    "url": "/codegame/static/media/roboto-condensed.light.cf4449c1.ttf"
+  }
+]);
